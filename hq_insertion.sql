f722c9574ee48248ec574cfbdf182c4f82cd8e20
Kevin Li <46378390+adularescence@users.noreply.github.com>
2020-03-01 10:25:11 -0500

    fix: localization for EN hq skills

--- hq_insertion.sql
+++ hq_insertion.sql
@@ -1,5 +1,5 @@
-INSERT INTO hq (hq_skill, morale, trade) VALUES ('Competent', -0.05, 0);
+INSERT INTO hq (hq_skill, morale, trade) VALUES ('Capable Assistance', -0.05, 0);
 INSERT INTO hq (hq_skill, morale, trade) VALUES ('S.W.E.E.P.', -0.05, 0);
-INSERT INTO hq (hq_skill, morale, trade, notes) VALUES ('Cooperation Agreement', 0, 7, 'Trade Efficiency Does Not Stack');
-INSERT INTO hq (hq_skill, morale, trade, notes) VALUES ('Little MIssy', 0, 7, 'Trade Efficiency Does Not Stack');
-INSERT INTO hq (hq_skill, morale, trade, notes) VALUES ('Ability & Integrity', -0.05, 0, 'Per "Lungman Guard Bureau" Opeator');
+INSERT INTO hq (hq_skill, morale, trade, notes) VALUES ('Agreement', 0, 7, 'Trade Efficiency Does Not Stack');
+INSERT INTO hq (hq_skill, morale, trade, notes) VALUES ('Missy', 0, 7, 'Trade Efficiency Does Not Stack');
+INSERT INTO hq (hq_skill, morale, trade, notes) VALUES ('Both Ability and Political Integrity', -0.05, 0, 'Per "Lungman Guard Bureau" Opeator');

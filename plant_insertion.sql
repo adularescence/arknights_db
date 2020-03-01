87912b1312c82cf37d0320ce0da23ab8c1fb1d25
Kevin Li <46378390+adularescence@users.noreply.github.com>
2020-02-29 21:02:58 -0500

    feat: add EN power plant skills

---
+++ plant_insertion.sql
@@ -0,0 +1,7 @@
+INSERT INTO plant VALUES ('Backup Generator', 10);
+INSERT INTO plant VALUES ('Thermal Energy alpha', 10);
+INSERT INTO plant VALUES ('Equipment Maintenance', 15);
+INSERT INTO plant VALUES ('Static Field', 20);
+INSERT INTO plant VALUES ('Pulsing Arc alpha', 10);
+INSERT INTO plant VALUES ('Pulsing Arc beta', 20);
+INSERT INTO plant VALUES ('High Temperature Recharge', 15);

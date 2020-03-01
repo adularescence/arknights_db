f049b21a527cc1f7fd904373d97c2e287134acce
Kevin Li <46378390+adularescence@users.noreply.github.com>
2020-02-29 20:58:44 -0500

    feat: add EN hq skills

---
+++ hq_insertion.sql
@@ -0,0 +1,5 @@
+INSERT INTO hq (hq_skill, morale, trade) VALUES ('Competent', -0.05, 0);
+INSERT INTO hq (hq_skill, morale, trade) VALUES ('S.W.E.E.P.', -0.05, 0);
+INSERT INTO hq (hq_skill, morale, trade, notes) VALUES ('Cooperation Agreement', 0, 7, 'Trade Efficiency Does Not Stack');
+INSERT INTO hq (hq_skill, morale, trade, notes) VALUES ('Little MIssy', 0, 7, 'Trade Efficiency Does Not Stack');
+INSERT INTO hq (hq_skill, morale, trade, notes) VALUES ('Ability & Integrity', -0.05, 0, 'Per "Lungman Guard Bureau" Opeator');

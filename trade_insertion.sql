831c0d3acd3dcb397547682b0940f1fbfe511b63
Kevin Li <46378390+adularescence@users.noreply.github.com>
2020-02-29 21:35:24 -0500

    feat: add EN trading post skills

---
+++ trade_insertion.sql
@@ -0,0 +1,123 @@
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Order Management Alpha',
+        10,
+        1,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Order Distribution Alpha',
+        20,
+        0,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Order Distribution Beta',
+        30,
+        0,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Negotiation',
+        0,
+        3,
+        -0.25
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Supply Management',
+        25,
+        1,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Communication',
+        30,
+        0,
+        -0.25
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Karlan Commercial Alpha',
+        15,
+        1,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Karlan Commercial Beta',
+        15,
+        2,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Penguin Logistics Alpha',
+        20,
+        0,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Penguin Logistics Beta',
+        30,
+        0,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale, notes)
+    VALUES (
+        'Feud',
+        65,
+        0,
+        0.3,
+        'When Stationed With Lappland'
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale, notes)
+    VALUES (
+        'Tacit Understanding',
+        0,
+        0,
+        -0.3,
+        'When Stationed With Exusiai'
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale, notes)
+    VALUES (
+        'Hidden Purpose Alpha',
+        0,
+        2,
+        -0.1,
+        'When Stationed With Texas'
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale, notes)
+    VALUES (
+        'Hidden Purpose Beta',
+        0,
+        4,
+        -0.1,
+        'When Stationed With Texas'
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Mission Guarantee',
+        30,
+        1,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Logistics Expert',
+        35,
+        0,
+        0
+    );
+INSERT INTO trade (trade_skill, efficiency, capacity, morale)
+    VALUES (
+        'Lord of Karlan',
+        20,
+        3,
+        0
+    );

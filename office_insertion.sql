80856ef55156e149ba9408c5dfec51d2b7f54da1
Kevin Li <46378390+adularescence@users.noreply.github.com>
2020-03-01 10:13:56 -0500

    feat: add EN office skills

---
+++ office_insertion.sql
@@ -0,0 +1,4 @@
+INSERT INTO office VALUES ('Personnel Management Alpha', 20);
+INSERT INTO office VALUES ('Catastrophe Messenger Alpha', 30);
+INSERT INTO office VALUES ('Catastrophe Messenger Beta', 45);
+INSERT INTO office VALUES ('Psychology', 40);

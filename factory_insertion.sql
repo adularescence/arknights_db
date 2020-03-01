046f23d7a8d1e6529cf4835e791adfbe45631a66
Kevin Li <46378390+adularescence@users.noreply.github.com>
2020-02-29 21:19:53 -0500

    feat: add EN factory skills

---
+++ factory_insertion.sql
@@ -0,0 +1,209 @@
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Metal Crafts Alpha',
+        30,
+        0,
+        0,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Metal Crafts Beta',
+        35,
+        0,
+        0,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Combat Directive Recordings',
+        0,
+        30,
+        0,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Originium Crafts Alpha',
+        0,
+        0,
+        30,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Originium Crafts Beta',
+        0,
+        0,
+        35,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Warehouse Organization Alpha',
+        10,
+        10,
+        10,
+        6,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Warehouse Organization Beta',
+        10,
+        10,
+        10,
+        10,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Standardization Alpha',
+        15,
+        15,
+        15,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Standardization Beta',
+        25,
+        25,
+        25,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Standardization Beta',
+        25,
+        25,
+        25,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale, notes)
+    VALUES (
+        'Impatient',
+        25,
+        25,
+        25,
+        0,
+        0,
+        'Starts At 20% Efficiency, Gain 1% Efficiency Per Hour Until Max'
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale, notes)
+    VALUES (
+        'Slow Starter',
+        25,
+        25,
+        25,
+        0,
+        0,
+        'Starts At 15% Efficiency, Gain 2% Efficiency Per Hour Until Max'
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale, notes)
+    VALUES (
+        'Troublesome Creator',
+        25,
+        25,
+        25,
+        -12,
+        0.2
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Originium Crafts Alpha',
+        0,
+        0,
+        30,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Scavenger',
+        0,
+        0,
+        0,
+        8,
+        -0.25
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Geologist',
+        0,
+        0,
+        35,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Rhine Technology Alpha',
+        15,
+        15,
+        15,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Rhine Technology Beta',
+        25,
+        25,
+        25,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Craftsmanship Alpha',
+        -5,
+        -5,
+        -5,
+        16,
+        -0.15
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Craftsmanship Beta',
+        -5,
+        -5,
+        -5,
+        18,
+        -0.25
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Boxing Guide Recordings',
+        0,
+        35,
+        0,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Meeboo: Manufacturing',
+        30,
+        30,
+        30,
+        0,
+        0
+    );
+INSERT INTO factory (factory_skill, metal, record, originium, capacity, morale)
+    VALUES (
+        'Volcanologist',
+        0,
+        0,
+        35,
+        0,
+        0
+    );

be193a7b5b09765db805b78d0efcfbecef226be3
Kevin Li <46378390+adularescence@users.noreply.github.com>
2020-03-01 10:29:00 -0500

    fix: localization for EN power plant skills

--- plant_insertion.sql
+++ plant_insertion.sql
@@ -1,7 +1,7 @@
-INSERT INTO plant VALUES ('Backup Generator', 10);
-INSERT INTO plant VALUES ('Thermal Energy alpha', 10);
+INSERT INTO plant VALUES ('Backup Energy', 10);
+INSERT INTO plant VALUES ('Heat Charge Alpha', 10);
 INSERT INTO plant VALUES ('Equipment Maintenance', 15);
-INSERT INTO plant VALUES ('Static Field', 20);
-INSERT INTO plant VALUES ('Pulsing Arc alpha', 10);
-INSERT INTO plant VALUES ('Pulsing Arc beta', 20);
-INSERT INTO plant VALUES ('High Temperature Recharge', 15);
+INSERT INTO plant VALUES ('Electrostatic Field', 20);
+INSERT INTO plant VALUES ('Pulsed Arc Blpha', 10);
+INSERT INTO plant VALUES ('Pulsed Arc Beta', 20);
+INSERT INTO plant VALUES ('Superheat Charge', 15);
